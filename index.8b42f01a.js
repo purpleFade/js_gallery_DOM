7bef3f8743237607535611550a3cf907a488f6bc
purplefade <tarovykanton@gmail.com>
2024-01-16 15:51:14 +0200

    make build

---
+++ index.8b42f01a.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery__list");e.addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");if(r&&e.contains(r)){var l=document.getElementById("largeImg"),n=r.getAttribute("href");n&&(l.src=n)}});//# sourceMappingURL=index.8b42f01a.js.map
+
+//# sourceMappingURL=index.8b42f01a.js.map
